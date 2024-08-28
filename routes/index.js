0ada94ff27e4d53979553f8bd5bbcde2a0184e03
saurabhpra295 <saurabhprakash295@gmail.com>
2024-08-28 10:44:04 +0530

    message

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var router = express.Router();
-const user = require("../models/userSchema")
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userSchema");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-// router.get("/createuser", (req, res, next) => {
-//   res.render('createUser', { title: 'Create User' })
-// });
-
-// router.post('/create', function(req, res, next) {
-//   const newuser = new user(req.body);
-//   newuser.save();
-//   res.render('index', user);
-// });
-// router.get('/create', function(req, res, next) {
-//   const newuser = new user(req.body);
-//   newuser.save();
-//   res.render('index', user);
-// });
+router.get("/", async(req,res)=> {
+  try {
+    const user = await User.find();
+    res.render("index", {users : user})
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
 
 
 
-module.exports = router;
+router.get("/createuser", (req,res)=> {
+  res.render("createuser")
+})
+
+router.post("/create", async(req,res)=> {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.redirect("/")
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
+
+module.exports =router
